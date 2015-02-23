49500e8bafbb99540f6d3f282d5cf9a6f35d0495
Steven <s.huang2407@gmail.com>
2015-02-22 17:49:59 -0800

    new config

--- crud/config/db.php
+++ crud/config/db.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST','localhost');
+define('DB_HOST','23.239.11.107');
 define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('DB_DATABASE','crud');
+define('DB_PASSWORD','q1w2e3AA');
+define('DB_DATABASE','we510_crud');
 define('BASE_URL', '/crud/');
